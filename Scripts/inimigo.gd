aa0c654b211ab267d189753154a1359ab880cdef
Rayan Raddatz <rayandematos@gmail.com>
2024-10-01 21:24:22 -0300

    agora real oficial inimigos naquelas

---
+++ Scripts/inimigo.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+# Called when the node enters the scene tree for the first time.
+const SPEED = 60
+var direction := 1
+
+@onready var player: CharacterBody2D = %Player
+@onready var parede_esq: RayCast2D = $ParedeEsq
+@onready var parede_dir: RayCast2D = $ParedeDir
+@onready var chao: RayCast2D = $chao
+
+const MOVEMENT_SPEED = Vector2(200.0, -400.0)
+const ACCELERATION = 15
+const FRICTION = 30
+
+var vida := 3
+var maxvida := 3
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor(): velocity += get_gravity() * delta
+	
+	if player.position.x < position.x: direction = -1
+	else: if player.position.x > position.x: direction = 1
+	else: direction = 0
+	
+	if direction: velocity.x = velocity.move_toward(MOVEMENT_SPEED * direction, ACCELERATION).x
+	
+	if direction == -1: $AnimatedSprite2D.flip_h = true
+	if direction == 1: $AnimatedSprite2D.flip_h = false
+	# sprites de andar e ficar parado
+	if direction != 0: $AnimatedSprite2D.play("walk")
+	else: $AnimatedSprite2D.play("idle")
+	
+	move_and_slide()
+	#if direction != 0: 	position.x += direction * SPEED * delta
+
+
+func _on_dano_body_entered(body: Node2D) -> void:
+	# Ativar o dano
+	print("dano")
+	if body.name == 'Player': pass # Replace with function body.
